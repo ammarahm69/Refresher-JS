3c19ec00b2c7a85f8ad7730fb7a0a00f83405d53
Ammar Ahmed <ammar69aa59@gmail.com>
2024-05-17 02:51:02 -0700

    object refrsher

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ function chat() {
         msgoutput.innerHTML += `I'm fine .`
     }
 }
+
+let bikeList = {
+    suzuki: {
+        Hayabusa: {
+            model: 2010,
+            enginepower: '1200cc',
+            avg: '18 km/hr'
+        },
+    },
+    honda: {
+        motorBike: {
+            model: 1989,
+            enginepower: '70 cc',
+            avg: '6 km/hr'
+        },
+    },
+    kawasaki: {
+        NinjaH2R: {
+            model: 2017,
+            enginepower: '2000 cc',
+            avg: '60 km/hr',
+        },
+    }
+
+}
+for (var key in bikeList) {
+    console.log(key);
+    for (var key1 in bikeList[key]) {
+        console.log(key1);
+        console.log(bikeList[key][key1]);
+    }
+}
